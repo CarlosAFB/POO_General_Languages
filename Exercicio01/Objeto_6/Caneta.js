fc4cb7c4c5e177d9f0cd1112087d10a41f37d823
CarlosAFB <carlosalexandre5670@gmail.com>
2021-07-22 08:34:30 -0300

    Update

--- Exercicio01/Objeto_6/Caneta.js
+++ Exercicio01/Objeto_6/Caneta.js
@@ -1,9 +1,42 @@
 function Caneta(){
  
     constructor(cor, marca, carga, ponta, tapada){
-        this.cor = cor
-        this.marca = marca
-        this.carga = carga
-        this.tapada = tapada
+        setCor(cor)
+        setMarca(marca)
+        setPonta(ponta)
+        setCarga(carga)
+        setTampada(tampada)
+    }
+    
+    relatorio(){
+      console.log("Nome: ${this._cor}")
+      console.log("Marca: ${this._marca}")
+      console.log("Nome: ${this._cor}")
+      console.log("Nome: ${this._cor}")
+      console.log("Nome: ${this._cor}")
+    }
+    
+    set setCor(cor){
+      this._cor = cor
+    }
+    
+    set setMarca(marca){
+      this._marca = marca
+    }
+    
+    set setCarga(carga){
+      this._carga = carga
+    }
+    
+    set setPonta(ponta){
+      this._ponta = ponta
+    }
+    
+    set setTampada(tampada){
+      this._tapada = tampada
+    }
+    
+    get cor(){
+      return this._cor
     }
 }
